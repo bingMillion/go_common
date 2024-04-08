cd39a4417d4e1e6000f26101cf0eb8f82efc4cae
郑炳贤 <bingxian.zheng@transsion.com>
2024-04-08 21:36:04 +0800

    update

--- pkg/awsoss/download.go
+++ pkg/awsoss/download.go
@@ -1,6 +1,7 @@
 package awsoss
 
 import (
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -8,16 +9,22 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"os"
+	"path"
+	"strings"
+	"time"
 )
 
-// Download
-// bucketName: 桶名称
+// Download 根据sql查询athena api，查出的结果保存在目标s3桶中。并下载。
 // region 区域，例如 us-west-2
 // sql: 要执行的sql语句
-// ossFilePath: athena会将查询结果保存到一个oss路径上。例如s3://your-bucket/your-prefix
+// s3ResultDir: s3的结果路径。如"s3://XXXX/testDir"。 XXX为桶名，testDir为桶里的文件名
 // localPath: 希望保存的文件名，带扩展名。例如xxx.csv
-// ossFileDir: 会存在bucketName捅内的 这个路径下
-func Download(ak, sk, bucketName, region, sql, ossFilePath, localPath, ossFileDir string) (err error) {
+func Download(ak, sk, region, sql, s3ResultDir, localPath string) (err error) {
+	s3Path := strings.TrimPrefix(s3ResultDir, "s3://")
+	bucketName, dir := path.Split(s3Path)
+	bucketName = strings.TrimSuffix(bucketName, "/")
+	dir = strings.TrimSuffix(dir, "/")
+
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String(region),
 		Credentials: credentials.NewStaticCredentials(ak, sk, ""),
@@ -26,13 +33,21 @@ func Download(ak, sk, bucketName, region, sql, ossFilePath, localPath, ossFileDi
 	input := &athena.StartQueryExecutionInput{
 		QueryString: aws.String(sql),
 		ResultConfiguration: &athena.ResultConfiguration{
-			OutputLocation: aws.String(ossFilePath),
+			OutputLocation: aws.String(s3ResultDir),
 		},
 	}
 
-	output, err := svc.StartQueryExecution(input)
+	output, err := svc.StartQueryExecution(input) // 只是启动查询，查询仍需要一些时间
 	if err != nil {
 		return err
+	}
+
+	time.Sleep(30 * time.Second)
+	output2, err := svc.GetQueryExecution(&athena.GetQueryExecutionInput{
+		QueryExecutionId: output.QueryExecutionId,
+	})
+	if *output2.QueryExecution.Status.State != "SUCCEEDED" {
+		return errors.New(*output2.QueryExecution.Status.StateChangeReason)
 	}
 
 	// 下载查询结果
@@ -45,8 +60,8 @@ func Download(ak, sk, bucketName, region, sql, ossFilePath, localPath, ossFileDi
 
 	_, err = downloader.Download(file,
 		&s3.GetObjectInput{
-			Bucket: aws.String(bucketName),                                           // 替换为你的 S3 桶名
-			Key:    aws.String(ossFileDir + "/" + *output.QueryExecutionId + ".csv"), // Athena 会将查询结果保存为 QueryExecutionId.csv
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(dir + "/" + *output.QueryExecutionId + ".csv"), // 下载路径为目录/QueryExecutionId.csv
 		})
 
 	if err != nil {
